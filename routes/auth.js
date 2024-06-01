e3806dfbb3e688de23f9dcdb73b960e22420b747
Suliman Hakimi <122719578+SulimanHakimi@users.noreply.github.com>
2024-06-01 21:24:00 +0430

    add try catch for func

--- routes/auth.js
+++ routes/auth.js
@@ -4,28 +4,42 @@ const passport = require("passport");
 const CLIENT_URL = "http://localhost:3000";
 
 router.get("/login/success", (req, res) => {
-  if (req.user) {
-    res.status(200).json({
-      success: true,
-      message: "successfull",
-      user: req.user,
-      //   cookies: req.cookies
-    });
+  try {
+    if (req.user) {
+      res.status(200).json({
+        success: true,
+        message: "successfull",
+        user: req.user,
+        //   cookies: req.cookies
+      });
+    }
+  } catch (error) {
+    console.log(error)
   }
 });
 
 router.get("/login/failed", (req, res) => {
+ try {
   res.status(401).json({
     success: false,
     message: "failure",
   });
+ } catch (error) {
+  console.log(error)
+
+ }
 });
 
 router.get("/logout", (req, res) => {
-  req.logout(req.user, (err) => {
-    if (err) return next(err);
-    res.redirect(CLIENT_URL);
-  });
+  try {
+    req.logout(req.user, (err) => {
+      if (err) return next(err);
+      res.redirect(CLIENT_URL);
+    });
+  } catch (error) {
+    console.log(error)
+
+  }
 });
 
 router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
