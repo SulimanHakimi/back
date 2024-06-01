d80c5bcb7d6dbba198d16032745041b0247a6f2f
Suliman Hakimi <122719578+SulimanHakimi@users.noreply.github.com>
2024-06-01 11:19:30 +0430

    update

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(passport.session());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://front-theta-mocha.vercel.app/",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
