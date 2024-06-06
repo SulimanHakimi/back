c7e864550c72d08734c01e3aa4d4c9b9f25234c2
Suliman Hakimi <122719578+SulimanHakimi@users.noreply.github.com>
2024-06-06 23:10:12 +0430

    allow all origin

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(passport.session());
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    origin: "*",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
